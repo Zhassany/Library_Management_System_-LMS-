2691c2a7c6662852f370549f7fad3fbd26f273a2
zHassani <abcd@gmail.com>
2024-09-03 11:38:07 +0430

    create Member class

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Book:
         if all(ch.isalpha() or ch.isspace() for ch in name):
             self.__author = name
         else:
-            raise ValueError("The author's name must contain only letters and space")
+            raise ValueError("The author's name must contain only letter and space")
 
     @property
     def isbn(self):
@@ -25,9 +25,8 @@ class Book:
         if number.isdigit():
             self.__isbn = number
         else:
-            raise ValueError("The isbn must contain only digits")
+            raise ValueError("The isbn must contain only digit")
 
-    
     def __str__(self):
         return f'title: {self.title}    author: {self.author}'
 
@@ -42,3 +41,34 @@ class Book:
             self.available = True
         else:
             raise AssertionError(f'{self.title} is already available')
+        
+class Member:
+    def __init__(self, name, member_id):
+        self.name = name
+        self.member_id = member_id
+        self.borrowed_books = []
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name_inp):
+        if all(ch.isalpha() or ch.isspace() for ch in name_inp):
+            self.__name = name_inp
+        else:
+            raise ValueError("The member's name must contain only letter and space")
+
+    def __str__(self):
+        return f'name: {self.name}    member_id: {self.member_id}'
+    
+    def borrow_book(self, book):
+        if isinstance(book, Book):
+            self.borrowed_books += [book]
+            book.borrow()
+        else:
+            raise TypeError('The input of the borrow_book function must be an object of the Book class')
+    
+    def return_book(self, book):
+        self.borrowed_books.remove(book)
+        book.return_book()
