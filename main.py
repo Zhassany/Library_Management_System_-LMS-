31a9c9239e3261406deed963a56de23cfefe73e5
zHassani <abcd@gmail.com>
2024-09-06 11:57:46 +0430

    add test and repository address

--- main.py
+++ main.py
@@ -137,3 +137,117 @@ class Library:
                 raise AssertionError(f'{isbn} not found')
         else:
             raise AssertionError(f'{member_id} not found')
+
+# ایجاد کتاب‌ها
+book1 = Book("1984", "جورج اورول", "1234567890")
+book2 = Book("کشتن مرغ مقلد", "هارپر لی", "0987654321")
+
+# ایجاد کتابخانه و اضافه کردن کتاب‌ها
+library = Library()
+library.add_book(book1)
+library.add_book(book2)
+
+# ثبت یک عضو
+member = Member("آلیس", "M001")
+library.register_member(member)
+
+# امانت دادن کتاب به عضو
+library.issue_book("M001", "1234567890")
+
+# بازگرداندن کتاب
+library.return_book("M001", "1234567890")
+
+# ----------------------------------------------------------------------------------------------------
+"""
+library = Library()
+
+while True:
+    print('1) add book \n'
+          '2) register member \n'
+          '3) lending book \n'
+          '4) return book \n'
+          '5) Show library books \n'
+          '6) Show library members \n'
+          '7) Show the borrowed books of a member \n'
+          '8) exit')
+
+    while True:
+        number = int(input('enter a number from the above list: '))
+        if number in range(1, 9):
+            break
+        
+    match number:
+        case 1:
+            while True:
+                title_book = input('\nenter the title of the book or "done" to finish: ')
+                if title_book == 'done':
+                    break
+                author_book = input('enter the author of the book: ')
+                isbn_book = input('enter the isbn of the book: ')
+                
+                try:
+                    book = Book(title_book, author_book, isbn_book)
+                    library.add_book(book)
+                except ValueError as ve:
+                    print(ve, '\n')
+                except TypeError as te:
+                    print(te, '\n')
+                except AssertionError as ae:
+                    print(ae, '\n')
+        case 2:
+            while True:
+                name_member = input('\nenter the name of the member or "done" to finish: ')
+                if name_member == 'done':
+                    break
+                id_member = input('enter the id of the member: ')
+                
+                try:
+                    member = Member(name_member, id_member)
+                    library.register_member(member)
+                except ValueError as ve:
+                    print(ve, '\n')
+                except TypeError as te:
+                    print(te, '\n')
+                except AssertionError as ae:
+                    print(ae, '\n')
+        case 3:
+            while True:
+                id_member = input('enter the id of the member or "done" to finish: ')
+                if id_member == 'done':
+                        break
+                isbn_book = input('enter the isbn of the book: ')
+                try:
+                    library.issue_book(id_member, isbn_book)
+                except AssertionError as ae:
+                    print(ae, '\n')
+                except TypeError as te:
+                    print(te, '\n')
+        case 4:
+            while True:
+                id_member = input('enter the id of the member or "done" to finish: ')
+                if id_member == 'done':
+                        break
+                isbn_book = input('enter the isbn of the book: ')
+                try:
+                    library.return_book(id_member, isbn_book)
+                except AssertionError as ae:
+                    print(ae, '\n')
+                except ValueError as ve:
+                    print(ve, '\n')
+        case 5:
+            print(*library.books)
+        case 6:
+            print(*library.members)
+        case 7:
+            id_member = input('enter the id of the member: ')
+            for x in library.members:
+                if id_member is x.member_id:
+                    print(*x.borrowed_books)
+                else:
+                    print('member not found')
+        case 8:
+            break
+"""
+# ----------------------------------------------------------------------------------------------------
+
+# https://github.com/Zhassany/Library_Management_System_-LMS-.git
